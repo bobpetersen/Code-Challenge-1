9c92b71259ad7faaf84f21a1d8a5663473265604
bobpetersen <33petersen@gmail.com>
2018-04-16 14:36:53 -0500

    here we gooooooo

--- client.js
+++ client.js
@@ -11,17 +11,19 @@ function onReady(){
         clickCount++;
         console.log(clickCount);
         updateDomCount();
-        //$('.ducati').append('<p>Test</p>');
         
-
-
     function updateDomCount(){
         $('#genButtonCount').text(clickCount);
-        $('.ducati').append('<div>Test</div>');
+        //$('.ducati').append('<div>Test</div>');
         $('.bultaco').append('<div></div>');
         $('#motoGuzzi').append('<div></div>');
+        $('.ducati').append('<div>'+'<button>Swap</button>'+'<button>Delete</button>'+'</div>');
+    }
+
+    //function createDivBtn(){
+        //$('.ducati').append('<div>'+'<button>Swap</button>'+'</div>');
     }
 
     }  
-} // end on ready
+ // end on ready
 
